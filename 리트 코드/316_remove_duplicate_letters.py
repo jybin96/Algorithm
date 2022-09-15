546f14642ab62de86f2e70779671b6201696bac5
jybin96 <jybin96@changwon.ac.kr>
2022-09-15 14:37:23 +0900

    2022/09/15

---
+++ 리트 코드/316_remove_duplicate_letters.py
@@ -0,0 +1,15 @@
+import collections
+
+s = str(input())
+stack, counter, seen = [], collections.Counter(s), set()
+
+for char in s:
+    counter[char] -= 1
+    if char in seen:
+        continue
+    while stack and char < stack[-1] and counter[stack[-1]] > 0:
+        seen.remove(stack.pop())
+    stack.append(char)
+    seen.add(char)
+
+print(''.join(stack))
